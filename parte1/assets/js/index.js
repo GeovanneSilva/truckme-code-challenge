b8aea7463b309822c2876631f80a640bed706015
Geovanne Silva <geovanne.08@gmail.com>
2024-07-13 12:43:36 -0300

    finalizando a pagina com o formulario e sua validação, parte 1 finalizada

--- parte1/assets/js/index.js
+++ parte1/assets/js/index.js
@@ -10,3 +10,36 @@ function moreText() {
     verMais.textContent = "Ver Mais";
   }
 }
+
+function validateForm() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var subject = document.getElementById("subject").value;
+  var message = document.getElementById("message").value;
+  var emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+  if (name.trim() === "") {
+    alert("Por favor, preencha o campo Nome.");
+    return false;
+  }
+
+  if (email.trim() === "") {
+    alert("Por favor, preencha o campo Email.");
+    return false;
+  } else if (!email.match(emailPattern)) {
+    alert("Por favor, insira um endereço de email válido.");
+    return false;
+  }
+
+  if (subject.trim() === "") {
+    alert("Por favor, preencha o campo Assunto.");
+    return false;
+  }
+
+  if (message.trim() === "") {
+    alert("Por favor, preencha o campo Mensagem.");
+    return false;
+  }
+  alert("Formulario enviado com sucesso!");
+  return true; // Se todas as validações passarem, o formulário pode ser enviado
+}
