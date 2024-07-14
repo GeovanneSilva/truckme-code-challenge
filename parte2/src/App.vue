b53280f0ba8137a826082b6f5a10c70d04508480
Geovanne Silva <geovanne.08@gmail.com>
2024-07-14 00:45:18 -0300

    finalizando estilização da barra inicial e configurações iniciais do vuetify

--- parte2/src/App.vue
+++ parte2/src/App.vue
@@ -1,32 +1,58 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="orange"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="./assets/logo.png"
+          transition="scale-transition"
+          width="150"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        text
+      >
+        <span>Home</span>
+      </v-btn>
+      <v-btn
+      text
+      >
+        <span>Sobre</span>
+      </v-btn>
+      <v-btn
+      text
+      >
+        <span>Serviço</span>
+      </v-btn>
+      <v-btn
+      text
+      >
+        <span>Contato</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
